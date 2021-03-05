c4fed1acfc7fa321f8e51e524826e68dc5e2dc02
William Benton <willb@nvidia.com>
2021-03-04 18:41:19 -0600

    start.sh:  use --packages to specify plugin

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 pipenv run spark-submit --driver-memory 16G --master="local[*]" ./generate.py
+pipenv run spark-submit --driver-memory 16G --master="local[*]" --packages com.nvidia:rapids-4-spark_2.12:0.3.0,ai.rapids:cudf:0.17 --conf spark.plugins=com.nvidia.spark.SQLPlugin ./do-etl.py

fe76a8dba45b1e305f9f016ee4c61ea9983f8e1d
William Benton <willb@nvidia.com>
2021-03-05 14:02:56 -0600

    generalized start script

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 pipenv run spark-submit --driver-memory 16G --master="local[*]" ./generate.py
-pipenv run spark-submit --driver-memory 16G --master="local[*]" --jars cudf-0.17-cuda11.jar,rapids-4-spark_2.12-0.3.0.jar --conf spark.plugins=com.nvidia.spark.SQLPlugin ./do-etl.py
+pipenv run spark-submit --driver-memory 16G --master="local[*]" --jars cudf-*.jar,rapids-4-spark_2.12-*.jar --conf spark.plugins=com.nvidia.spark.SQLPlugin ./do-etl.py

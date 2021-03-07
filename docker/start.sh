7a719ad6d19cfb3f98ba213f1b44af1bdef5f1f2
William Benton <willb@nvidia.com>
2021-03-06 21:50:33 -0600

    slightly more resilient docker start script

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+export JARPATH=$(realpath $(dirname $0))
+
 pipenv run spark-submit --driver-memory 16G --master="local[*]" ./generate.py
-pipenv run spark-submit --driver-memory 16G --master="local[*]" --jars cudf-*.jar,rapids-4-spark_2.12-*.jar --conf spark.plugins=com.nvidia.spark.SQLPlugin ./do-etl.py
+pipenv run spark-submit --driver-memory 16G --master="local[*]" --jars ${JARPATH}/cudf-*.jar,${JARPATH}/rapids-4-spark_2.12-*.jar --conf spark.plugins=com.nvidia.spark.SQLPlugin ./do-etl.py

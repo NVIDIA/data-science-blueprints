2a7e4216e106fe6bb31cb63ef8d5a3d75598b6b9
William Benton <willb@nvidia.com>
2021-03-16 12:39:50 -0500

    WIP on start.sh for larger runs

--- docker/start.sh
+++ docker/start.sh
@@ -5,10 +5,13 @@ set -x
 export JARPATH=$(realpath $(dirname $0))
 
 if [ x$"LIGHTRUN" != "x" ]; then
-    DUP_TIMES="--dup-times 500"
+    DUP_TIMES="--dup-times 5000"
 else
     DUP_TIMES="--dup-times 100"
 fi
 
-pipenv run spark-submit --driver-memory 16G --master="local[*]" --conf spark.sql.analyzer.failAmbiguousSelfJoin=false ./generate.py ${DUP_TIMES} 
-pipenv run spark-submit --driver-memory 16G --master="local[*]" --jars ${JARPATH}/cudf-*.jar,${JARPATH}/rapids-4-spark_2.12-*.jar --conf spark.plugins=com.nvidia.spark.SQLPlugin --conf spark.sql.adaptive.enabled=true --conf spark.rapids.sql.batchSizeBytes=1073741824 --conf spark.rapids.sql.concurrentGpuTasks=4 --conf spark.default.parallelism=256 --conf spark.rapids.sql.explain=NOT_ON_GPU --conf spark.rapids.sql.decimalType.enabled=true --conf spark.rapids.sql.variableFloatAgg.enabled=true ./do-analytics.py --log-level INFO
+mkdir tmp
+export SPARK_TEMP=$(pwd)/tmp
+
+pipenv run spark-submit --driver-memory 128G --master="local[*]" --conf spark.sql.analyzer.failAmbiguousSelfJoin=false --conf spark.local.dir=${SPARK_TEMP} ./generate.py ${DUP_TIMES} 
+pipenv run spark-submit --driver-memory 128G --master="local[*]" --jars ${JARPATH}/cudf-*.jar,${JARPATH}/rapids-4-spark_2.12-*.jar --conf spark.plugins=com.nvidia.spark.SQLPlugin --conf spark.sql.adaptive.enabled=true --conf spark.rapids.sql.batchSizeBytes=1073741824 --conf spark.rapids.sql.concurrentGpuTasks=4 --conf spark.default.parallelism=256 --conf spark.rapids.sql.explain=NOT_ON_GPU --conf spark.rapids.sql.decimalType.enabled=true --conf spark.rapids.sql.variableFloatAgg.enabled=true --conf spark.local.dir=${SPARK_TEMP} --conf spark.eventLog.enabled=True ./do-analytics.py --log-level INFO

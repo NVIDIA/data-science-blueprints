ccf995892ad90a420a4c4bbd6aaa60bb2d9c205d
William Benton <willb@nvidia.com>
2021-03-15 21:32:30 -0500

    analytics smoke test

--- docker/start.sh
+++ docker/start.sh
@@ -5,10 +5,10 @@ set -x
 export JARPATH=$(realpath $(dirname $0))
 
 if [ x$"LIGHTRUN" != "x" ]; then
-    DUP_TIMES="--dup-times 10000"
+    DUP_TIMES="--dup-times 1000"
 else
     DUP_TIMES="--dup-times 100"
 fi
 
-pipenv run spark-submit --driver-memory 16G --master="local[*]" ./generate.py ${DUP_TIMES}
-pipenv run spark-submit --driver-memory 16G --master="local[*]" --jars ${JARPATH}/cudf-*.jar,${JARPATH}/rapids-4-spark_2.12-*.jar --conf spark.plugins=com.nvidia.spark.SQLPlugin --conf spark.sql.adaptive.enabled=true --conf spark.rapids.sql.batchSizeBytes=1073741824 --conf spark.rapids.sql.concurrentGpuTasks=4 --conf spark.default.parallelism=256 ./do-etl.py
+pipenv run spark-submit --driver-memory 16G --master="local[*]" --conf spark.sql.analyzer.failAmbiguousSelfJoin=false ./generate.py ${DUP_TIMES} 
+pipenv run spark-submit --driver-memory 16G --master="local[*]" --jars ${JARPATH}/cudf-*.jar,${JARPATH}/rapids-4-spark_2.12-*.jar --conf spark.plugins=com.nvidia.spark.SQLPlugin --conf spark.sql.adaptive.enabled=true --conf spark.rapids.sql.batchSizeBytes=1073741824 --conf spark.rapids.sql.concurrentGpuTasks=4 --conf spark.default.parallelism=256 --conf spark.rapids.sql.explain=NOT_ON_GPU ./do-analytics.py --log-level INFO
